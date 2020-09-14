47399b51a56206c013de7e02745400cbbd214136
Nandini Kapoor <55255331+costheta-z@users.noreply.github.com>
2020-09-14 15:03:28 +0530

    Update musicjs.js

--- musicjs.js
+++ musicjs.js
@@ -49,7 +49,7 @@ window.addEventListener('load', () => {
                         }
                         if (index == 19) {
                             index = 0;
-                            break;
+                            return;
                         } else stateChange();
                     })
                     //}
